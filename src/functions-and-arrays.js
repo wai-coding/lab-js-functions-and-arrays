48aa867148a09664f1d2f39c78bccf1865fb0ddb
Luis Castro <luiscastrosounds@gmail.com>
2025-11-20 22:50:28 +0000

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,74 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+// console.log(maxOfTwoNumbers(5, 10));
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
 
+  let longestWord = words[0];
 
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
+// console.log(findLongestWord(words));
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+// console.log(sumNumbers(numbers));
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
+function averageNumbers(numbers2) {
+  if (numbers2.length === 0) {
+    return 0;
+  } else {
+    return sumNumbers(numbers2) / numbers2.length;
+  }
+}
+// console.log(averageNumbers(numbers2));
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, word) {
+  if (words2.length === 0) {
+    return null;
+  } else {
+    for (let i = 0; i < words2.length; i++) {
+      if (words2[i] === word) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+// console.log(doesWordExist(words2,"truth"));
